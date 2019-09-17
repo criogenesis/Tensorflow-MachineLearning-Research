8cebd5787bda1bcfc0b2d3516918c15fd48f9709
criogenesis <ztuttle99@gmail.com>
2019-09-17 13:17:31 -0400

    added object tester

---
+++ object_tester.py
@@ -0,0 +1,21 @@
+
+
+import Multi_Object_Detect as mod
+import pyautogui as pyag
+import time
+
+testlist = ['cursor', 'chrome']
+
+result_dict = mod.recieve_object(testlist)
+
+for key, value in result_dict.items():
+    left = value[0]
+    right = value[1]
+    top = value[2]
+    bottom = value[3]
+    print(key, value)
+    if key == "chrome":
+        average_width = (left + right)/2
+        average_height = (top + bottom)/2
+time.sleep(2)
+pyag.moveTo(average_width, average_height, duration=1)
